d0ae500e4d16f2a0a1ee9a9cd9f7d0bde49859e6
Avdeenko Ekaterina <avdeenqoeka@gmail.com>
2023-05-30 14:51:47 +0200

    average_AvdeenkoEkaterina

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -1,12 +1,31 @@
 package com.epam.rd.autotasks;
 
+
 import java.util.Scanner;
 
 public class Average {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // Use Scanner methods to read input
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println();
+        int value = 0, sum = 0, count = 0;
+        while((value = scan.nextInt()) != 0)
+        {
+            count++;
+            sum = sum + value;
+        }
+        System.out.println(sum / count);
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
